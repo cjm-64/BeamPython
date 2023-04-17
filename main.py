2a3c0d80ec3b4ba9f94da0debc23687cd63aa434
Chris <christopher.morris7164@gmail.com>
2023-04-17 16:26:34 -0400

    Decreased fps to increase resolution

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uvc
 import cv2
 
 from typing import NamedTuple
+import time
 
 import os
 
@@ -19,23 +20,25 @@ def getCameraInfo():
     listOfCamInfo = []
     for device in uvc.device_list():
         cap = uvc.Capture(device["uid"])
-
         print(cap.name)
         if int(cap.name[-1]) > 1:
-            listOfCamInfo.append(CameraSpec(
-                name=cap.name,
-                width=cap.available_modes[10][0],
-                height=cap.available_modes[10][1],
-                fps=cap.available_modes[10][2],
-                bandwidth_factor=1.6,
-            )
-            )
+            print("World Cam")
+            # Uncomment to include world cam
+            # listOfCamInfo.append(CameraSpec(
+            #     name=cap.name,
+            #     width=cap.available_modes[10][0],
+            #     height=cap.available_modes[10][1],
+            #     fps=cap.available_modes[10][2],
+            #     bandwidth_factor=1.6,
+            # )
+            # )
         else:
+            capmode = 0
             listOfCamInfo.append(CameraSpec(
                 name=cap.name,
-                width=cap.available_modes[5][0],
-                height=cap.available_modes[5][1],
-                fps=cap.available_modes[5][2],
+                width=cap.available_modes[capmode][0],
+                height=cap.available_modes[capmode][1],
+                fps=cap.available_modes[capmode][2],
                 bandwidth_factor=1.6,
             )
             )
@@ -51,41 +54,60 @@ def initCameraFromList(devices, camera: CameraSpec):
             for mode in capture.available_modes:
                 if mode[:3] == camera[1:4]:  # compare width, height, fps
                     capture.frame_mode = mode
+                    setCamAttrs(capture)
                     return capture
             else:
                 logging.warning(
                     f"None of the available modes matched: {capture.available_modes}"
                 )
             capture.close()
+            print("Cap Closed")
 
-def main():
-    camInfo = getCameraInfo()
-    print(camInfo)
-    devices = uvc.device_list()
-    cameras = {spec: initCameraFromList(devices, spec) for spec in camInfo}
-    print(cameras.items())
+def setCamAttrs(cam):
+    print(cam.name)
+    controls_dict = dict([(c.display_name, c) for c in cam.controls])
+    print(controls_dict['Gamma'])
+    print(controls_dict['Brightness'])
+    print(controls_dict['Auto Exposure Mode'])
+    controls_dict['Gamma'].value = 200
+    controls_dict['Brightness'].value = 0
+    controls_dict['Auto Exposure Mode'].value = 1
+    print(controls_dict['Gamma'])
+    print(controls_dict['Brightness'])
+    print(controls_dict['Auto Exposure Mode'])
+    return cam
 
-    for spec, cam in cameras.items():
-        print(spec.name)
-        if int(spec.name[-1]) < 2:
-            controls_dict = dict([(c.display_name, c) for c in cam.controls])
-            controls_dict['Auto Exposure Mode'].value = 1
-            controls_dict['Gamma'].value = 200
-        else:
-            controls_dict = dict([(c.display_name, c) for c in cam.controls])
-            controls_dict['Auto Exposure Mode'].value = 1
-    print(len(cameras))
-    print("\n\n")
-    print(type(cameras))
-    print(cameras.keys())
-    print("\n\n")
-
-    for x in range(200):
-        for spec, cam in cameras.items():
+# def updateCamSettings(Cameras):
+#     print("UCS")
+#     for spec, cam in Cameras.items():
+#         print(spec.name)
+#         if int(spec.name[-1]) < 2:
+#             controls_dict = dict([(c.display_name, c) for c in cam.controls])
+#             print(controls_dict)
+#             print(controls_dict['Gamma'])
+#             print(controls_dict['Brightness'])
+#             print(controls_dict['Auto Exposure Mode'])
+#             controls_dict['Auto Exposure Mode'].value = 1
+#             controls_dict['Gamma'].value = 200
+#             print(controls_dict)
+#             print(controls_dict['Gamma'])
+#             print(controls_dict['Brightness'])
+#             print(controls_dict['Auto Exposure Mode'])
+#         else:
+#             controls_dict = dict([(c.display_name, c) for c in cam.controls])
+#             controls_dict['Auto Exposure Mode'].value = 1
+#     return Cameras
+def openStream(streamTime, camDict):
+    # camDict = updateCamSettings(camDict)
+    initTime = time.perf_counter()
+    currTime = 0
+    while currTime < streamTime:
+        print(currTime)
+        for spec, cam in camDict.items():
             if int(spec.name[-1]) < 2:
                 print(spec.name)
                 try:
-                    frame = cam.get_frame_robust()
+                    frame = cam.get_frame(timeout=0.1)
                 except TimeoutError:
                     print("TimeoutError")
                     pass
@@ -95,14 +117,28 @@ def main():
                 except uvc.StreamError as err:
                     logging.debug(f"Failed to get a frame for {spec}: {err}")
                 else:
-                    data = frame.bgr if hasattr(frame, "bgr") else frame.gray
-                    if frame.data_fully_received:
-                        cv2.imshow(spec.name, data)
-                        cv2.waitKey(1)
-    for cam in cameras.values():
+                    cv2.imshow(spec.name, frame.bgr)
+                    cv2.waitKey(1)
+        currTime = time.perf_counter() - initTime
+
+    for cam in camDict.values():
         cam.close()
 
+def main():
+    camInfo = getCameraInfo()
+    print(camInfo)
+    devices = uvc.device_list()
+    camDict = {spec: initCameraFromList(devices, spec) for spec in camInfo}
+    print(camDict.items())
 
+    print(len(camDict))
+    print("\n\n")
+    print(type(camDict))
+    print(camDict.keys())
+    print(camDict.values())
+    print("\n\n")
+
+    openStream(5, camDict)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
